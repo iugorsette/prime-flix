7e8023f868be995bf9324234c8ce5bf47b33d44c
Iugor Sette Pereira <iugorsette@gmail.com>
2022-12-27 20:18:15 -0300

    aula 41

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,38 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import api from "../../services/api";
+import './home.css'
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
 
-  useEffect(()=>{
-    async function loadFilme(){
-        const response = await api.get("/movie/now_playing",{
-            params:{
-                api_key:'c1f9343990348acff5761df661d1fa66',
-                language: "pt-BR",
-                page:1,
-            }
-        })
-        console.log(response.data.results.slice(0,10))
+  useEffect(() => {
+    async function loadFilme() {
+      const response = await api.get("/movie/now_playing", {
+        params: {
+          api_key: "c1f9343990348acff5761df661d1fa66",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+      setFilmes(response.data.results.slice(0, 10));
     }
-    loadFilme()
-  },[])
+    loadFilme();
+  }, []);
+
   return (
-    <div>
-      <h1> Welcome to Home Page</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+              <Link to={`/filmes/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
