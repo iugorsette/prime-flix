32c1b3c49871e5fa13b1f2b55a4aa8a88bf62b21
Iugor Sette Pereira <iugorsette@gmail.com>
2022-12-27 20:48:39 -0300

    aula 43

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,9 +1,54 @@
-function Filme(){
-    return(
-        <div>
-            <h1> Welcome to Movie Details </h1>
-        </div>
-    )
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import api from "../../services/api";
+
+function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setloading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "c1f9343990348acff5761df661d1fa66",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+        })
+        .catch(() => {
+          console.log(`filme nao encontrado`);
+        });
+    }
+    loadFilme();
+    setloading(false);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1> Carregando detalhes...</h1>
+      </div>
+    );
+  }
+  return (
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+      <h3>Sinopse</h3>
+      <p>{filme.overview}</p>
+
+        <strong>{filme.vote_average}</strong>
+
+    </div>
+  );
 }
 
 export default Filme;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -38,7 +38,7 @@ function Home() {
             <article key={filme.id}>
               <strong>{filme.title}</strong>
               <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
-              <Link to={`/filmes/${filme.id}`}>Acessar</Link>
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
             </article>
           );
         })}
